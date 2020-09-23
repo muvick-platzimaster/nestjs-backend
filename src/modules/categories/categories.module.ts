a1c32771447955509eeee81142cdc8b2dd4da072
Axel Espinosa <axeldavid45@gmail.com>
2020-09-23 18:23:10 -0500

    Added category schema and model

--- src/modules/categories/categories.module.ts
+++ src/modules/categories/categories.module.ts
@@ -1,10 +1,13 @@
 import { HttpModule, Module } from '@nestjs/common';
 import { CategoriesController } from './categories.controller';
 import { CategoriesService } from './categories.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { CategorySchema } from './schemas/category.schema';
 
 @Module({
-  imports: [HttpModule],
+  imports: [HttpModule, MongooseModule.forFeature([{ name: 'category', schema: CategorySchema }])],
   controllers: [CategoriesController],
-  providers: [CategoriesService]
+  providers: [CategoriesService],
 })
-export class CategoriesModule {}
+export class CategoriesModule {
+}
