51afa1cc080de02d4ca146a403b944e55fdeba3a
Axel Espinosa <axeldavid45@gmail.com>
2020-09-24 21:23:20 -0500

    Documentacion de metodo para listar categorias de peliculas

--- src/modules/categories/categories.controller.ts
+++ src/modules/categories/categories.controller.ts
@@ -1,18 +1,39 @@
 import { Controller, Get } from '@nestjs/common';
 import { CategoriesService } from './categories.service';
+import { ApiOkResponse, ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 
+@ApiTags('Categories')
 @Controller('categories')
 export class CategoriesController {
   constructor(private _categoriesService: CategoriesService) {
   }
 
   @Get('/movies')
+  @ApiOperation({ summary: 'Retrieves movies categories (genres)' })
+  @ApiOkResponse({
+    schema: {
+      type: 'object',
+      properties: {
+        genres: {
+          type: 'array',
+          example: [{id: 88, name: 'Action'}]
+        }
+      }
+    }
+  })
+
   async moviesCategories() {
-    return this._categoriesService.listMoviesCategories()
+    return this._categoriesService.listMoviesCategories();
   }
 
   @Get('/series')
+  @ApiOperation({ summary: 'Retrieves series categories (genres)' })
+  @ApiResponse({
+    status: 200,
+    description: 'An array with all the series categories available in our API',
+  })
+
   async seriesCategories() {
-    return this._categoriesService.listSeriesCategories()
+    return this._categoriesService.listSeriesCategories();
   }
 }
